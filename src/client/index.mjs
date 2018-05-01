58b6b9be2794f42d0e7afac0b82c99675026cee5
Slađan Ristić <sladiri@users.noreply.github.com>
2018-05-01 22:57:25 +0200

    refactoring: Move dispatch to own file.

--- src/client/index.mjs
+++ src/client/index.mjs
@@ -4,17 +4,20 @@ import { hyper as render, wire } from "hyperhtml/esm";
 import { app } from "../app";
 // @ts-ignore
 import { acceptor } from "../app/acceptor";
-
-const state = {
-  posts: [],
-};
-const actionsInProgress = new Set();
-const accept = acceptor(state);
+// @ts-ignore
+import { dispatcher } from "./dispatch";
 
 (async () => {
+  assert.ok(document);
+
+  const state = {
+    posts: [],
+    title: document.title,
+  };
+
   const container = document.querySelector("#app");
   assert.ok(container);
-  state.title = document.title;
+
   await render(container)`${app({
     render,
     wire,
@@ -22,13 +25,18 @@ const accept = acceptor(state);
     dispatch: (hook, ...data) => e => hook.call(null, e, ...data),
   })}`;
   await new Promise(res => setTimeout(res, 3000)); // Test delay
-  await replayIntermediateEvents();
+  await replayIntermediateEvents({ dispatch: globalDispatch({ state }) });
 })().catch(error => {
   console.error("app index error", error);
 });
 
-const replayIntermediateEvents = async () => {
-  // TODO signal to prevent actions during this phase?
+const globalDispatch = ({ state }) =>
+  dispatcher({
+    actionsInProgress: new Set(),
+    accept: acceptor(state),
+  });
+
+const replayIntermediateEvents = async ({ dispatch }) => {
   console.log("replaying start");
   window["dispatcher"].dispatch = dispatch;
   for (const action of window["dispatcher"].toReplay) {
@@ -37,37 +45,4 @@ const replayIntermediateEvents = async () => {
   }
   window["dispatcher"].toReplay = null;
   console.log("replaying end");
-};
-
-const dispatch = async (action, isInitialReplay) => {
-  try {
-    if (!isInitialReplay && window["dispatcher"].toReplay) {
-      console.warn("DISPATCH: Abort, non-empty window.dispatcher.toReplay.");
-      return;
-    }
-
-    if (actionsInProgress.size) {
-      console.warn(
-        `DISPATCH: Abort, proposals in progress [${[
-          ...actionsInProgress.values(),
-        ]}] ...`,
-      );
-      return;
-    }
-
-    let actionId = Math.random();
-    while (actionsInProgress.has(actionId)) {
-      actionId = Math.random();
-    }
-    actionsInProgress.add(actionId);
-    console.log(`DISPATCH: awaiting proposal [${actionId}] ...`, action);
-
-    const proposal = await action;
-    const newState = await accept(proposal);
-    actionsInProgress.delete(actionId);
-
-    console.log(`DISPATCH: acceptor done [${actionId}]`, proposal, newState);
-  } catch (error) {
-    console.error("DISPATCH error:", error);
-  }
 };
