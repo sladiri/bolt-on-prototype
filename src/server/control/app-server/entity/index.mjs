126c2f6d4a8dc17a07c3e39aff47fe3f212f3d6d
Slađan Ristić <sladiri@users.noreply.github.com>
2018-05-29 21:11:58 +0200

    project: move file

--- src/server/control/app-server/entity/index.mjs
+++ src/server/control/app-server/entity/index.mjs
@@ -1,4 +1,1 @@
-export const posts = [
-    { title: "post a", summary: "a summary", content: "hi there" },
-    { title: "post b", summary: "b summary", content: "bye all" },
-];
+export { posts } from "./posts";
