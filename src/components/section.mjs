abb2e7ba5bd6b10336c77b06ae334cd3ef5b12d9
Slađan Ristić <sladiri@users.noreply.github.com>
2018-06-03 00:36:09 +0200

    project: remove section component

--- src/components/section.mjs
+++
@@ -1,9 +0,0 @@
-export const section = ({ node, id, heading, topLevel = false }) => props => {
-    const child = props.render`
-        <h1 id=${id} tabindex="-1">${heading}</h1>
-        ${node(props)}
-        `;
-    return topLevel
-        ? [props.render`<section>${child}</section>`, id, heading]
-        : [props.render`<div role="main">${child}</div>`, id, heading];
-};
