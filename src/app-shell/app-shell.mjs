05c3182ad6b2a92141c32a0e67d34514bad8de8f
Slađan Ristić <sladiri@users.noreply.github.com>
2018-06-02 20:03:14 +0200

    ux: add aria landmarks to top level section

--- src/app-shell/app-shell.mjs
+++ src/app-shell/app-shell.mjs
@@ -21,11 +21,11 @@ export const appShell = props => {
     );
     return render`
         <ul class="skipLinks">${skipLinks}</ul>
-        <header></header>
-        <nav></nav>
+        <header role="banner"></header>
+        <nav role="navigation"></nav>
         <h1>${title}, ${name}</h1>
-        <main>${content}</main>
-        <footer></footer>
+        <main role="main">${content}</main>
+        <footer role="contentinfo"></footer>
         `;
 };
 
